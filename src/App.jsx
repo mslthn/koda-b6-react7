c33b6e9f39c0eee446961d8d0a20877b3bab71bb
mslthn <msulthon1909@gmail.com>
2026-02-06 15:05:43 +0700

    first init project

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useRef } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todo, setTodo] = useState([])
+  const [newTask, setNewTask] = useState("")
+  const inputRef = useRef(null)
+
+  const handleAddTask = (e) => {
+    if(newTask.trim() !== ""){
+      setTodo([...todo, {completed:false, Text:newTask, Date: new Date().toLocaleDateString()}])
+      setNewTask("")
+    }
+  }
+
+  const handleComplete = (taskIndex) => {
+    setTodo(todo => 
+      todo.map((task, index) => 
+        index === taskIndex ? {...task, completed: !task.completed} : task
+      )
+    )
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="flex flex-col items-center">
+      <div className="bg-[#BFC6C4] mt-5 h-150 rounded-2xl">
+        <h1 className="text-2xl flex justify-end mr-5 mt-3">TODO</h1>
+        <form className="bg-[#E8E2D8] mx-3 h-12 w-96 rounded-xl flex items-center px-5 gap-5">
+          <input className="outline-none bg-transparent w-full text-gray-700" 
+            type="text" value={newTask} onChange={(e) => setNewTask(e.target.value)} onKeyDown={(e)=> e.key === 'Enter' && handleAddTask } placeholder='Add item'/>
+          <button className="bg-[#ACBAC4] w-8 h-8 rounded-full text-white font-bold"
+            type="submit">
+            {/* // onClick={() => setTodo((todo) => )}> */}
+            {/* // + {todo} */}
+          +</button>
+        </form>
+        <div>
+
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
